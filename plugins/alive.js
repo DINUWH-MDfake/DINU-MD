6c2b0b23f208272aa06a579a011fc08c795c2514
DINUWH-MD <dinethnethruvan772@gmail.com>
2025-01-11 11:04:26 +0530

    Update alive.js

--- plugins/alive.js
+++ plugins/alive.js
@@ -1,17 +1,17 @@
-const config = require('../config')
-const {cmd , commands} = require('../command')
+const express = require('express');
+const app = express();
 
-cmd({
-    pattern: "alive",
-    desc: "Check bot online or no.",
-    category: "main",
-    filename: __filename
-},
-async(conn, mek, m,{from, quoted, body, isCmd, command, args, q, isGroup, sender, senderNumber, botNumber2, botNumber, pushname, isMe, isOwner, groupMetadata, groupName, participants, groupAdmins, isBotAdmins, isAdmins, reply}) => {
-try{
-return await conn.sendMessage(from,{image: {url: config.ALIVE_IMG},caption: config.ALIVE_MSG},{quoted: mek})
-}catch(e){
-console.log(e)
-reply(`${e}`)
-}
-})
+// Simple route to display bot is alive
+app.get('/', (req, res) => {
+    res.send('WhatsApp Bot is Alive!');
+});
+
+// Keep the server alive with a ping route
+app.get('/ping', (req, res) => {
+    res.send('pong');
+});
+
+// Set up the server to listen on a port
+app.listen(process.env.PORT || 3000, () => {
+    console.log('Bot is alive and running!');
+});
