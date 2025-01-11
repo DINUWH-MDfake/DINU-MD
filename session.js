96cff71729f28c708448296f25b215599ab56bfe
DINUWH-MD <dinethnethruvan772@gmail.com>
2025-01-11 11:27:13 +0530

    Create session.js

---
+++ session.js
@@ -0,0 +1,12 @@
+{
+  "keys": {
+    "clientID": "YOUR_CLIENT_ID",
+    "token": "YOUR_TOKEN"
+  },
+  "state": {
+    "chats": [],
+    "contacts": [],
+    "messages": [],
+    "sessions": []
+  }
+}
