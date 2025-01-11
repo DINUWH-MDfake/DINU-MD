420d5c22c24f6c20d576b8d208040eac7c0dcd0e
DINUWH-MD <dinethnethruvan772@gmail.com>
2025-01-11 11:18:08 +0530

    Update config.js

--- config.js
+++ config.js
@@ -1,5 +1,4 @@
 module.exports = {
-  sessionId: 'SAxwXRRb#FNMrjBA3ZqUCbLdOAloY_iyvNUiG3Log1mtLjBVwh2Y',  // Session ID, Bot එක හොඳට සුරක්ෂිතව ලියා ගැනීම.
   botName: 'Dinuwh Bot',            // Bot එකේ නම.
   prefix: '.',                     // Bot එකට පසු කිරීමේ Prefix එක (උදාහරණ: !help, !ping).
   ownerNumber: '94728896048', // Bot එකේ Owner phone number.
