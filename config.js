8f1059621b601d431b47d52b8770a5ad785ffb39
DINUWH-MD <dinethnethruvan772@gmail.com>
2025-01-11 11:17:16 +0530

    Update config.js

--- config.js
+++ config.js
@@ -4,4 +4,5 @@ module.exports = {
   prefix: '.',                     // Bot එකට පසු කිරීමේ Prefix එක (උදාහරණ: !help, !ping).
   ownerNumber: '94728896048', // Bot එකේ Owner phone number.
   welcomeMessage: 'Hello, I am Dinuwh Bot! How can I assist you?', // Welcome message for new chat.
+SESSION_ID: proccess.env.SESSION_ID || "FNMrjBA3ZqUCbLdOAloY_iyvNUiG3Log1mtLjBVwh2Y"
 };
